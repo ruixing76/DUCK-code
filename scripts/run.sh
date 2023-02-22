157ed730fe419461c04be04ce19713de67b099ce
joshchang0111 <j5521631@gmail.com>
2023-02-23 03:19:33 +0800

    ...

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 #Generate graph data for each dataset
 #python vgae_preprocessing.py DATASET
@@ -10,21 +10,21 @@ export CUDA_VISIBLE_DEVICES=1
 #python train.py MODEL_NAME DATASET
 
 ## Comment Tree
-#python train.py \
-#	--datasetName Twitter15 \
-#	--baseDirectory ./data \
-#	--n_classes 4 \
-#	--foldnum 0 \
-#	--mode CommentTree \
-#	--modelName Simple_GAT_BERT \
-#	--batchsize 4
-
-## CCCT (Comment Chain + Comment Tree)
 python train.py \
 	--datasetName Twitter15 \
 	--baseDirectory ./data \
 	--n_classes 4 \
 	--foldnum 0 \
 	--mode CommentTree \
-	--modelName CCCTNet \
+	--modelName Simple_GAT_BERT \
 	--batchsize 4
+
+## CCCT (Comment Chain + Comment Tree)
+#python train.py \
+#	--datasetName Twitter15 \
+#	--baseDirectory ./data \
+#	--n_classes 4 \
+#	--foldnum 0 \
+#	--mode CommentTree \
+#	--modelName CCCTNet \
+#	--batchsize 4
