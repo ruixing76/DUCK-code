96dcdb8fe9f581ba340f49f23e90e89f17261333
joshchang0111 <j5521631@gmail.com>
2023-02-28 15:45:18 +0800

    Update README.md

--- scripts/run.sh
+++ scripts/run.sh
@@ -26,20 +26,13 @@ for dataset in Twitter15 Twitter16 semeval2019
 do
 	if [ $dataset = semeval2019 ]; then
 		n_classes=3
-		dropout=0.5
-		lr_bert=2e-5
-		lr_gnn=3e-4
 	elif [ $dataset = Twitter15 ]; then
 		n_classes=4
-		dropout=0.5
-		lr_bert=2e-5
-		lr_gnn=3e-4
 	elif [ $dataset = Twitter16 ]; then
 		n_classes=4
-		dropout=0.5
-		lr_bert=2e-5
-		lr_gnn=3e-4
 	fi
+
+	lr_bert=2e-5
 
 	for lr_gnn in 1e-4 2e-4 4e-4
 	do
@@ -62,52 +55,42 @@ do
 					--dropout_gat $dropout \
 					--n_epochs 20 \
 					#--max_tree_len 40
-				
-				## CCCT (Comment Chain + Comment Tree)
-				#python train.py \
-				#	--datasetName Twitter15 \
-				#	--baseDirectory ./data \
-				#	--n_classes 4 \
-				#	--foldnum 0 \
-				#	--mode CommentTree \
-				#	--modelName CCCTNet \
-				#	--batchsize 4
 			done
 		done
 	done
 done
 
 ## Hyperparameters tuning
-for dataset in Twitter15 Twitter16 semeval2019
-do
-	if [ $dataset = semeval2019 ]
-	then
-		n_classes=3
-	else
-		n_classes=4
-	fi
-
-	fold=0
-	for lr_bert in 1e-5 2e-5 3e-5 4e-5 5e-5
-	do
-		for lr_gnn in 1e-4 2e-4 3e-4 4e-4
-		do
-			for dropout_gat in 0.2 0.3 0.4 0.5 0.6
-			do
-				## Comment Tree (BERT+GAT)
-				python train.py \
-					--datasetName $dataset \
-					--baseDirectory ./data \
-					--n_classes $n_classes \
-					--foldnum $fold \
-					--mode CommentTree \
-					--modelName Simple_GAT_BERT \
-					--batchsize $batch_size \
-					--learningRate $lr_bert \
-					--learningRateGraph $lr_gnn \
-					--dropout_gat $dropout_gat \
-					--n_epochs 10
-			done
-		done
-	done
-done
+#for dataset in Twitter15 Twitter16 semeval2019
+#do
+#	if [ $dataset = semeval2019 ]
+#	then
+#		n_classes=3
+#	else
+#		n_classes=4
+#	fi
+#
+#	fold=0
+#	for lr_bert in 1e-5 2e-5 3e-5 4e-5 5e-5
+#	do
+#		for lr_gnn in 1e-4 2e-4 3e-4 4e-4
+#		do
+#			for dropout_gat in 0.2 0.3 0.4 0.5 0.6
+#			do
+#				## Comment Tree (BERT+GAT)
+#				python train.py \
+#					--datasetName $dataset \
+#					--baseDirectory ./data \
+#					--n_classes $n_classes \
+#					--foldnum $fold \
+#					--mode CommentTree \
+#					--modelName Simple_GAT_BERT \
+#					--batchsize $batch_size \
+#					--learningRate $lr_bert \
+#					--learningRateGraph $lr_gnn \
+#					--dropout_gat $dropout_gat \
+#					--n_epochs 10
+#			done
+#		done
+#	done
+#done
